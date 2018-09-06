e98c2a2fd52c43c79d95b90fcedf3d2e902ea51a
descom <descom@win764b.descom.fi>
2018-09-06 11:35:50 +0200

    after second step

---
+++ WebSphereCommerceServerExtensionsLogic/src/com/ibm/commerce/sample/commands/MyNewController2Cmd.java
@@ -0,0 +1,8 @@
+package com.ibm.commerce.sample.commands;
+
+import com.ibm.commerce.command.ControllerCommand;
+
+public interface MyNewController2Cmd extends ControllerCommand{
+	
+static final String DEFAULT_COMMAND_CLASS_NAME = "com.ibm.commerce.sample.commands.MyNewController2CmdImpl";
+}
